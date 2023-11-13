18338afcc48162b0a8bf719c93583f10d3e05475
Nguyễn Đình Trưng <127390593+dinhtrungndt@users.noreply.github.com>
2023-11-13 16:33:29 +0700

    up load profile

--- lib/screens/bottom_tab_bar/cart/index.dart
+++ lib/screens/bottom_tab_bar/cart/index.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:shop_shoes/screens/bottom_tab_bar/home/header/Header.dart';
+import 'package:shop_shoes/screens/bottom_tab_bar/home/search/Search.dart';
 
 class CartScreen extends StatefulWidget {
   const CartScreen({super.key});
@@ -11,11 +11,142 @@ class CartScreen extends StatefulWidget {
 class _CartScreenState extends State<CartScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(15.0),
+        padding: const EdgeInsets.all(20.0),
         child: Column(
-          children: [Header()],
+          children: [
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image(
+                  image: AssetImage('assets/back_login.png'),
+                ),
+                Text(
+                  "Giỏ hàng",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 0, 95, 103)),
+                ),
+                Image(
+                  image: AssetImage('assets/home/account.png'),
+                ),
+              ],
+            ),
+            const Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: Search(),
+            ),
+            const Row(
+              children: [
+                Padding(padding: EdgeInsets.only(top: 50)),
+                Text(
+                  "3 Item",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 0, 95, 103)),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    const Row(
+                      children: [
+                        Image(
+                          image: AssetImage('assets/cart/item_01.png'),
+                          width: 130,
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "Nike Club Max",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 0, 95, 103)),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "\$584.95",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 0, 95, 103)),
+                            ),
+                            Text(
+                              "Blule",
+                              style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 0, 95, 103)),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    Stack(
+                      children: [
+                        Container(
+                          width: 300,
+                          height: 30,
+                          decoration: const BoxDecoration(
+                            color: Color.fromARGB(233, 0, 101, 233),
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10),
+                            ),
+                          ),
+                        ),
+                        const Row(
+                          children: [
+                            Padding(padding: EdgeInsets.only(left: 100)),
+                            Text(
+                              "+",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 255, 255, 255),
+                              ),
+                            ),
+                            Padding(padding: EdgeInsets.only(left: 25)),
+                            Text(
+                              "1",
+                              style: TextStyle(
+                                fontSize: 23,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 255, 255, 255),
+                              ),
+                            ),
+                            Padding(padding: EdgeInsets.only(left: 25)),
+                            Text(
+                              "-",
+                              style: TextStyle(
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromARGB(255, 255, 255, 255),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
         ),
       ),
     );
--- lib/screens/bottom_tab_bar/profile/index.dart
+++ lib/screens/bottom_tab_bar/profile/index.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shop_shoes/screens/bottom_tab_bar/home/header/Header.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -11,11 +10,76 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(15.0),
+        padding: const EdgeInsets.all(15.0),
         child: Column(
-          children: [Header()],
+          children: [
+            const Padding(padding: EdgeInsets.only(top: 10)),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image(image: AssetImage('assets/back_login.png')),
+                Text(
+                  "Hồ sơ",
+                  style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Color.fromARGB(255, 0, 95, 103)),
+                ),
+                Image(image: AssetImage('assets/profile/cart.png')),
+              ],
+            ),
+            const Padding(padding: EdgeInsets.only(top: 30)),
+            Container(
+              width: 350,
+              height: 200,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(20)),
+                color: Color.fromARGB(255, 101, 190, 255),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromARGB(255, 0, 0, 0),
+                    blurRadius: 10.0,
+                    spreadRadius: 0.0,
+                    offset: Offset(
+                      0.0,
+                      0.0,
+                    ),
+                  ),
+                ],
+              ),
+              child: const Column(
+                children: [
+                  Padding(padding: EdgeInsets.only(top: 15)),
+                  Image(
+                    image: AssetImage('assets/profile/avatar.png'),
+                  ),
+                  Padding(padding: EdgeInsets.only(top: 10)),
+                  Text(
+                    "Nguyễn Đình Trưng",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Color.fromARGB(255, 0, 95, 103)),
+                  ),
+                  Text(
+                    "nguyendinhtrung.it@gmai.com",
+                    style: TextStyle(
+                        fontSize: 14, color: Color.fromARGB(255, 0, 95, 103)),
+                  ),
+                ],
+              ),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 50)),
+            const Image(image: AssetImage('assets/profile/caidatthongtin.png')),
+            const Padding(padding: EdgeInsets.only(top: 20)),
+            const Image(image: AssetImage('assets/profile/dathang.png')),
+            const Padding(padding: EdgeInsets.only(top: 20)),
+            const Image(image: AssetImage('assets/profile/hotro.png')),
+            const Padding(padding: EdgeInsets.only(top: 20)),
+            const Image(image: AssetImage('assets/profile/dangxuat.png')),
+          ],
         ),
       ),
     );
